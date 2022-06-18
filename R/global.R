31ec9c8dc939d91853f61d1304099e0ad721a93a
James Ross <ross.james94@gmail.com>
2022-06-18 04:34:12 -0500

    more package testing

--- R/global.R
+++ R/global.R
@@ -1,3 +1,11 @@
 library(shiny)
 library(ggplot2)
 datasets <- c("economics", "faithfuld", "seals")
+
+#' App Start
+#' Start App
+#' @export
+#' 
+startApp <- function(){
+  shinyApp(ui = ui ,server = server)
+}
